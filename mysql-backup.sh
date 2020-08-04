2eaf4bfe1fff0efe0de94ccf8aaf74282d8a95b5
Farhan Sabbir Siddique <fsabbir@gmail.com>
2020-08-04 21:05:38 +0600

    added mysql db dump too because of obvious reasons

--- mysql-backup.sh
+++ mysql-backup.sh
@@ -1,10 +1,15 @@
-#!/bin/bash -x
+#!/bin/bash
 LOGFILE=/monitoring-db/mysql-server/backup.log
 echo "Zabbix database structure backup started at `date`" >> $LOGFILE
 TABLES=""
-for filename in `find /monitoring-db/data/zabbix/ -type f -name "*ibd" ! -name "*\#P\#*" -size -1200M -printf "%p "`; do filename=${filename%%'.ibd'}; TABLES="$TABLES ${filename#'/monitoring-db/data/zabbix/'}"; done;
-/monitoring-db/mysql-server/bin/mysqldump --dump-date -a -R -d -S /monitoring-db/mysql-server/mysql.sock zabbix > /mnt/$HOSTNAME/zabbix_db_struct-`date +%d%m%Y%H%M%S`
-/monitoring-db/mysql-server/bin/mysqldump --dump-date -a -R -S /monitoring-db/mysql-server/mysql.sock zabbix $TABLES > /mnt/$HOSTNAME/zabbix_config_dump-`date +%d%m%Y%H%M%S`
+for filename in `find /monitoring-db/data/zabbix/ -type f -name "*ibd" ! -name "*\#P\#*" -size -1200M -printf "%p "`;
+do
+        filename=${filename%%'.ibd'};
+        TABLES="$TABLES ${filename#'/monitoring-db/data/zabbix/'}";
+done;
+/monitoring-db/mysql-server/bin/mysqldump --dump-date -a -R -d -S /monitoring-db/mysql-server/mysql.sock  zabbix  > /mnt/$HOSTNAME/zabbix_db_struct-`date +%d%m%Y%H%M%S`
+/monitoring-db/mysql-server/bin/mysqldump --dump-date -a -R -S /monitoring-db/mysql-server/mysql.sock --databases mysql > /mnt/$HOSTNAME/mysql_data_dump-`date +%d%m%Y%H%M%S`
+/monitoring-db/mysql-server/bin/mysqldump --dump-date -a -R -S /monitoring-db/mysql-server/mysql.sock zabbix $TABLES > /mnt/$HOSTNAME/zabbix_data_dump-`date +%d%m%Y%H%M%S`
 
 echo "Zabbix database full backup started at `date`" >> $LOGFILE
 rsync --recursive --links --acls --owner --group --progress --human-readable --compress --times --perms --update /monitoring-db/data/* "/mnt/$HOSTNAME/data/"
