530f2d5a069b0c15545e9b862cd3dd1c0ae5e924
Farhan Sabbir Siddique <fsabbir@gmail.com>
2020-08-05 01:33:57 +0600

    working out the partition distribution

--- partition_mgt.py
+++ partition_mgt.py
@@ -1,11 +1,29 @@
-#!/usr/bin/env python3
+#!/bin/env python3
 
 import datetime
 import math
+import sys
 import time
 import calendar
 
+TABLES_TO_PARTITION = ["history","history_uint","history_log","history_text"]
+# mountpoints where partitions will be distributed across
+# idea behind is, 
+#  - partitions from same table from same month should not be on same location
+#  - partitions from different table for same period should not be in same location
+# Example: history_uint_aug2020_10 and history_text_august2020_10 should reside on different file-systems
+# Example: history_uint_aug2020_10 and history_uint_aug2020_20 must reside on different file-systems
+# So number of file-systems must be equal to the maximum number of partitions
+# Better to have a total of 12 file-systems and max of 10 partitions per month or
+# 6 file-systems and 4 partitions per table.
+# Must not be less than 6 file-systems. This is a requirement here and no debates will be encouraged.
+DATA_FILESYSTEMS = ["/data01","/data02","/data03","/data04","/data05","/data06","/data10","/data11","/data12","/data07","/data08","/data09"]
+
+
 def getPossiblePartitions(months_ahead_from_now=1,partitions_per_month=10):
+    if (partitions_per_month * len(TABLES_TO_PARTITION))%len(DATA_FILESYSTEMS) < 2 or len(DATA_FILESYSTEMS)<6:
+        print("Number of partitions must not be more than available file-systems for partitions or file-systems must be at least 6.")
+        sys.exit(1)
     ret = dict()
     dates = list()
     year = datetime.datetime.today().year
@@ -21,9 +39,16 @@ def getPossiblePartitions(months_ahead_from_now=1,partitions_per_month=10):
             continue
         dates.append(day)
     dates.append(days_in_month)
-    for date in dates:
-        unixtime = time.mktime(datetime.datetime(year=year,month=month,day=date,hour=23,minute=59,second=59).timetuple())
-        ret[(time.strftime("%b%d%Y",time.gmtime(unixtime))).lower()] = unixtime
+    for table in TABLES_TO_PARTITION:
+        ret[table] = dict()
+        for date in dates:
+            unixtime = time.mktime(datetime.datetime(year=year,month=month,day=date,hour=23,minute=59,second=59).timetuple())
+            ret[table][(time.strftime("%b%Y_%d",time.gmtime(unixtime))).lower()] = unixtime
     return ret
 
-print(getPossiblePartitions(1,10))
+partitions = (getPossiblePartitions(1,10))
+print(partitions)
+for partition,filesystem in zip(partitions.keys(),DATA_FILESYSTEMS):
+    for tables in (TABLES_TO_PARTITION):
+        partition_name = (tables + "_" + partition)
+        #print(partition_name + " -> " + filesystem)
